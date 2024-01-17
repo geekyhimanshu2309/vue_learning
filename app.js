ef18e4d1d71f2ee5b9ead825e758604752ae2c91
Himanshu <98638511+geekyhimanshu2309@users.noreply.github.com>
2024-01-18 01:13:09 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,19 +1,48 @@
+// Proxy: A figure that can be used to represent the value of something in a calculation
 const vm = Vue.createApp({
-    data(){
-        return {
-          firstName: "Himanshu",
-          url: "https://google.com",
-        };
+  data() {
+    return {
+      firstName: "Himanshu",
+      middleName: "",
+      lastName: "geek",
+      url: "https://google.com",
+      raw_url: '<a href="https://google.com" target="_blank">Google</a>',
+      age: 20,
+    };
+  },
+  methods: {
+    increment() {
+      this.age++;
     },
-    methods:{
-        fullName(){
-            return `${this.firstName.toUpperCase()}`
-        }
+    updateLastName(msg, event) {
+      // event.preventDefault();
+      console.log(msg);
+      this.lastName = event.target.value;
+    },
+    updateMiddleName(event) {
+      this.middleName = event.target.value;
+    },
+  },
+  computed: {
+    fullName() {
+      console.log("Full name computed property was called!!");
+      return `${this.firstName.toUpperCase()} ${this.middleName} ${
+        this.lastName
+      }`;
+    },
+  },
+  watch: {
+    age(newVal, oldVal){
+        setTimeout(() => {
+          this.age = 20
+        },3000)
     }
-}).mount('#app')
+  }
+}).mount("#app");
 
 // setTimeout(() => {
 //     vm.firstName = "bob";
 // },2000)
 
 // Proxy: A figure that can be used to represent the value of something in a calculation
+// Computed Properties- How to create computed properties and why they are useful
